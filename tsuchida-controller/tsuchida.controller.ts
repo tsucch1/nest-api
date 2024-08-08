2ecca855b1b992720e3501085984db1a74f3e37d
tsuchida <tsuchida.shimadai@gmail.com>
2024-08-08 19:34:47 +0900

    tsuchida-controllerの削除

--- tsuchida-controller/tsuchida.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('tsuchida')
-export class TsuchidaController {
- @Get()
-  findAll(): string {
-    return 'This action returns all cats';
-  }
-}
